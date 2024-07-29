35c5a8e929ad67bd06e71fe676a6103e08a41970
Andy Kolean <andykolean@gmail.com>
2024-07-29 02:40:34 -0400

    remove generated code

--- Foundation/PokemonGraphClient/Sources/Generated/Schema/CustomScalars/Jsonb.swift
+++
@@ -1,5 +0,0 @@
-import ApolloAPI
-
-extension GraphClient {
-    typealias Jsonb = String
-}
--- Foundation/PokemonGraphClient/Sources/Generated/Schema/SchemaConfiguration.swift
+++
@@ -1,8 +0,0 @@
-import ApolloAPI
-
-enum SchemaConfiguration: ApolloAPI.SchemaConfiguration {
-    static func cacheKeyInfo(for _: ApolloAPI.Object, object _: ApolloAPI.ObjectData) -> CacheKeyInfo? {
-        // Implement this function to configure cache key resolution for your schema types.
-        return nil
-    }
-}
